0901c682d859cff5dca6d3e61ac70dea7f52fe38
Chris Dzoba <Chris@Dzoba.com>
2023-05-04 20:34:20 -0700

    Fix desktop border

--- src/App.tsx
+++ src/App.tsx
@@ -169,7 +169,7 @@ function App() {
                 id="setA"
                 name="setA"
                 onChange={onChangeA}
-                className="pl-2 rounded-md border-2 border-slate-300 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="pl-2 rounded-md border-2 md:border-0 border-slate-300 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
@@ -184,7 +184,7 @@ function App() {
                 id="setB"
                 name="setB"
                 onChange={onChangeB}
-                className="pl-2 rounded-md border-2 border-slate-300 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="pl-2 rounded-md border-2 md:border-0 border-slate-300 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
@@ -199,7 +199,7 @@ function App() {
                 id="setC"
                 name="setC"
                 onChange={onChangeC}
-                className="pl-2 rounded-md border-2 border-slate-300 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="pl-2 rounded-md border-2 md:border-0 border-slate-300 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
@@ -214,7 +214,7 @@ function App() {
                 id="setD"
                 name="setD"
                 onChange={onChangeD}
-                className="pl-2 rounded-md border-2 border-slate-300 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="pl-2 rounded-md border-2 md:border-0 border-slate-300 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
