93cc48a5d2623c628c71a2c0407904956dff15d6
Chris Dzoba <Chris@Dzoba.com>
2023-05-07 07:59:42 -0700

    Pre push

--- src/App.tsx
+++ src/App.tsx
@@ -229,7 +229,10 @@ function App() {
             <button
               className="rounded-md mt-5 bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               style={{ width: 'fit-content' }}
-              onClick={handleClick}>
+              type="submit"
+              onClick={handleClick}
+            >
+
               Analyze
             </button>
           </div>
